b4a1495c9688d91147817f24657a26c25d44428f
IPS-030 <meetp-itpath@gmail.com>
2023-04-18 12:49:26 +0530

    Done: profile

--- app/src/main/java/com/ipsmeet/chatapp/dataclasses/SignInDataClass.kt
+++ app/src/main/java/com/ipsmeet/chatapp/dataclasses/SignInDataClass.kt
@@ -1,7 +1,6 @@
 package com.ipsmeet.chatapp.dataclasses
 
 data class SignInDataClass(
-    val profileImg: String?,
     val userName: String,
     val phoneNumber: String
 )
--- app/src/main/java/com/ipsmeet/chatapp/dataclasses/UserDataClass.kt
+++ app/src/main/java/com/ipsmeet/chatapp/dataclasses/UserDataClass.kt
@@ -2,7 +2,6 @@ package com.ipsmeet.chatapp.dataclasses
 
 data class UserDataClass(
     var key: String = "",
-    val profileImg: String? = "",
     val userName: String = "",
     val phoneNumber: String = ""
 )
